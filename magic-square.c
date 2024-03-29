f4b631307de9932e3c67943d4527b4584744e7f5
Mahito Tanaka <hacoman@asahinet.jp>
2024-03-29 21:39:18 +0900

    Delete a whitespace.

--- magic-square.c
+++ magic-square.c
@@ -7,7 +7,7 @@ int solved_answer (int *a, int *answer_a, int *kakutei, int *sum, int n,
   int ans=0;
   int i,j,k;
   for (j=0; j<n; j++)
-    for (i=0; i<n; i++) 
+    for (i=0; i<n; i++)
       if (a[j*n+i] == 0) {
 	if (total) {
 	  for (k=0; k<4; k++) {
